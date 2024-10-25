5b7d81fe5e29705ef4fe8e6d9b0faa58a8507dc2
Nagaya Makoto <makoto@longroof.jp>
2024-10-25 21:00:30 +0900

    [modify]mysql-conf

--- database/config.php
+++ database/config.php
@@ -2,8 +2,12 @@
 
 // Configuration for database connection
 
-$host       = getenv('DB_HOST');
-$username   = getenv('DB_USERNAME');
-$password   = getenv('DB_PASSWORD');
-$db_name     = getenv('DB_DATABASE');
+// $host       = getenv('DB_HOST');
+$host       = getenv('hal-nh24-ih15-nagaya241025-server');
+// $username   = getenv('DB_USERNAME');
+$username   = getenv('zddjbzyekm');
+// $password   = getenv('DB_PASSWORD');
+$password   = getenv('2vED7vPffhNk$rC0');
+// $db_name     = getenv('DB_DATABASE');
+$db_name     = getenv('hal-nh24-ih15-nagaya241025-database');
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
